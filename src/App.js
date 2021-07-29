0a9b3a11893cbd76b01a76992eacb4e9f8ad30af
rhish9h <rhish9h@gmail.com>
2021-07-29 12:21:40 +0530

    dummy landing page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi! You've reached Rhishabh Hattarki's newest experiment. He'll be migrating his wordpress portfolio here soon.
+        </p>
+
+        <p>
+          You can check out the old website here
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://rhishhatt.wordpress.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Old portfolio
         </a>
       </header>
     </div>
