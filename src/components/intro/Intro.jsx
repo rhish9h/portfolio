5ee50eff4db570ad01a77b2c9ad7f89fa9c61ea9
rhish9h <rhish9h@gmail.com>
2021-08-11 22:51:10 +0530

    added profile pic in intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,12 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className='intro' id='intro'>
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/profile_no_bg.png" alt="Profile" />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
