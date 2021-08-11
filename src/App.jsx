3605ddb03a813906062e5d8c3c001aed65822f84
rhish9h <rhish9h@gmail.com>
2021-08-11 21:31:46 +0530

    side bar menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Topbar from './components/topbar/Topbar';
 import Intro from './components/intro/Intro';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
+import Menu from "./components/menu/Menu";
 import './app.scss'
 import { useState } from "react";
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
 
       <div className="sections">
         <Intro/>
