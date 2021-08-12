78352f205b077492a4eaf64cdd82013e291e7e2b
rhish9h <rhish9h@gmail.com>
2021-08-12 15:32:58 +0530

    intro done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss"
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            strings: ["Developer", "Engineer", "Development Engineer"]
+        });
+    }, [])
+
     return (
         <div className='intro' id='intro'>
             <div className="left">
@@ -8,7 +22,16 @@ export default function Intro() {
                     <img src="assets/profile_no_bg.png" alt="Profile" />
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi, I'm</h2>
+                    <h1>Rhishabh Hattarki</h1>
+                    <h3>Software <span ref={textRef}></span></h3>
+                </div>
+                <a href="#projects">
+                    <ExpandMoreIcon className='downarrow'/>
+                </a>
+            </div>
         </div>
     )
 }
