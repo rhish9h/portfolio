101173a0de112f593ca3b0a41a2d9e800d18127c
rhish9h <rhish9h@gmail.com>
2021-08-14 21:04:27 +0530

    projects page

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,45 @@
 import "./projects.scss"
 
 export default function Projects() {
+    const projects = [
+        {
+            id: 1,
+            title: "Real Time IDS",
+            img: "assets/car_ids_ss.png",
+            link: "https://github.com/s3r-be/be-project"
+        },
+        {
+            id: 2,
+            title: "Apace Inventory Management System",
+            img: "assets/apace_inv_ss.png",
+            link: "https://github.com/rhish9h/apaceInventory"
+        },
+        {
+            id: 3,
+            title: "YouTube Data Analysis and Like Count Prediction",
+            img: "assets/youtube_data_ss.png",
+            link: "https://github.com/rhish9h/Youtube-data-analysis-and-like-count-prediction"
+        }
+    ]
     return (
         <div className='projects' id='projects'>
+            <h1>Projects</h1>
             
+            <div className="container">
+            {projects.map((d) => (
+                <a href={d.link} target="_blank" rel="noreferrer">
+                    <div className="item">
+                        <img
+                        src={d.img}
+                        alt={d.title}
+                        />
+                        <div className="title">
+                            <h3>{d.title}</h3>
+                        </div>
+                    </div>
+                </a>
+            ))} 
+            </div>
         </div>
     )
 }
