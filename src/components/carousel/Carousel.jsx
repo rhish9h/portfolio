dab7fc578061a17f26348126eb25eac3896bc1a2
rhish9h <rhish9h@gmail.com>
2022-10-02 17:25:12 -0700

    replaced list of projects with basic carousel

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./carousel.scss";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import Slide from "./slide/Slide";
+
+export default function Carousel ({data}) {
+    const [ curSlide, setCurSlide ] = useState(0);
+    const dataLen = data.length;
+
+    const nextSlide = () => {
+        setCurSlide((prev) => {
+            return (prev + 1) % dataLen;
+        });
+    }
+
+    const prevSlide = () => {
+        setCurSlide((prev) => {
+            return (prev + dataLen - 1) % dataLen;
+        });
+    }
+
+    if (!Array.isArray(data) || data.length <= 0) {
+        return null;
+    }
+
+    return (
+        <section className="slider">
+            <KeyboardArrowLeftIcon className="arrow left-arrow" onClick={prevSlide} />
+            <KeyboardArrowRightIcon className="arrow right-arrow" onClick={nextSlide} />
+            
+            {data.map((slide, index) => {
+                return (
+                    <Slide className={index === curSlide ? "active-slide" : "inactive-slide"} slide={slide} key={slide.id}/>
+                )
+            })}
+        </section>
+    )
+}
---
+++ src/components/carousel/slide/Slide.jsx
@@ -0,0 +1,16 @@
+import "./slide.scss";
+
+export default function Slide({className, slide}) {
+    return (
+        <div className={`slide ${className}`}>
+            <div className="image">
+                <img src={slide.img} alt={slide.title}/>
+            </div>
+            <div className="title">
+                <h3>
+                    {slide.title}
+                </h3>
+            </div>
+        </div>
+    )
+}
