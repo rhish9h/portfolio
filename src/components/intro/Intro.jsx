c39a9789d7c4103eeaecf3ed3ee0e9c785882c75
rhish9h <rhish9h@gmail.com>
2023-01-23 00:24:45 -0700

    resume download option added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,18 @@
 import "./intro.scss"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import FileDownloadDoneIcon from '@mui/icons-material/FileDownloadDone';
 import { init } from 'ityped';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Intro() {
 
     const textRef = useRef();
+    const [downloaded, setDownloaded] = useState(false);
+
+    const downloadClicked = () => {
+        setDownloaded(true);
+    }
 
     useEffect(() => {
         init(textRef.current, {
@@ -27,6 +34,15 @@ export default function Intro() {
                     <h2>Hi, I'm</h2>
                     <h1>Rhishabh Hattarki</h1>
                     <h3>Software <span ref={textRef}></span></h3>
+                </div>
+                <div className="resume">
+                    <a href="assets/Rhishabh_Hattarki.pdf" download="Rhishabh_Hattarki_Resume.pdf">
+                        <button onClick={downloadClicked}>
+                            {downloaded ? <FileDownloadDoneIcon/> : <FileDownloadIcon/>}
+                            &nbsp;
+                            RESUME
+                        </button>
+                    </a>
                 </div>
                 <a href="#projects">
                     <ExpandMoreIcon className='downarrow'/>
