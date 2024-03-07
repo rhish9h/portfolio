efb976018ee14f525174c1cc8c8f11bfb502a518
rhish9h <rhish9h@gmail.com>
2024-03-07 02:12:26 -0500

    basic top bar

---
+++ src/components/topBar/TopBar.tsx
@@ -0,0 +1,11 @@
+import styles from './TopBar.module.css';
+
+const TopBar = () => {
+    return (
+        <div className={styles.topBar}>
+            <div className={styles.logo}>rhish.in</div>
+        </div>
+    );
+}
+
+export default TopBar;
