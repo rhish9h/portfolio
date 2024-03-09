5df4e9bc3a59080e52fa991cbe681099f37ab0e8
rhish9h <rhish9h@gmail.com>
2024-03-08 19:41:59 -0500

    basic homepage

---
+++ src/components/homePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+    return (
+        <div className={styles.home}>
+            hi
+        </div>
+    );
+}
+
+export default HomePage;
