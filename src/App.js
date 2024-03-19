76d2b1e636f5cc497c9c8da68dff4ba41fcf5335
rhish9h <rhish9h@gmail.com>
2024-03-18 18:57:16 -0700

    full reset - basic react app - with particle js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Patterns from './components/patterns/Patterns';
+
+function App() {
+  return (
+    <div className="App">
+      <Patterns />
+    </div>
+  );
+}
+
+export default App;
